61051b4e64abb59f25326c91c41dcc428625e2c0
ricardo larrahondo <ricardolarrahondo2@hotmail.com>
2018-06-13 11:23:24 -0500

    add gh-pages branch

--- src/main/java/com/example/ricardo/questions/ResultMessagesQuestions.java
+++
@@ -1,8 +0,0 @@
-package com.example.ricardo.questions;
-
-public class ResultMessagesQuestions {
-
-    public static RegistrationQuestion registrationResultMessage(){
-        return new RegistrationQuestion();
-    }
-}
